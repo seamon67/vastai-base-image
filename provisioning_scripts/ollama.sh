e3d42829a2f2abe7cbbc26567a2fe760db907bb5
seamon67 <seamon67@gmail.com>
2025-10-27 02:37:36 +0530

    Added GPT OSS 120b

--- provisioning_scripts/ollama.sh
+++ provisioning_scripts/ollama.sh
@@ -69,6 +69,7 @@ ollama pull seamon67/Skyfall-4.1:31b-q8_0
 ollama pull gemma3:27b-it-q8_0
 ollama pull qwen3:32b-q8_0
 ollama pull seamon67/Magistral1.2-2509:24b-q8_0
+ollama pull gpt-oss:120b
 
 FAIL_STRINGS=(
     "Error: pull model manifest:"
